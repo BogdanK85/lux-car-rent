2034b7fc1f90eb89367182a3524b2f363dd4ad7a
BogdanK85 <126570087+BogdanK85@users.noreply.github.com>
2024-02-04 11:14:03 +0200

    add fetch cars

--- src/redux/fecth.js
+++ src/redux/fecth.js
@@ -1,29 +1,29 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://65bbb85152189914b5bcf1b1.mockapi.io';
+axios.defaults.baseURL = 'https://65bbb85152189914b5bcf1b1.mockapi.io';
 
-// export const LIMIT = 12;
+export const LIMIT = 12;
 
-// const getAllCars = async (_, thunkAPI) => {
-//   try {
-//     const response = await axios.get('/adverts');
-//     return response.data;
-//   } catch (error) {
-//     return thunkAPI.rejectWithValue(error.message);
-//   }
-// };
+const getAllCars = async (_, thunkAPI) => {
+  try {
+    const response = await axios.get('/adverts');
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+};
 
-// const getCars = async (_, thunkAPI) => {
-//   try {
-//     const response = await axios.get(`/adverts?page=${page}&limit=${LIMIT}`);
-//     return response.data;
-//   } catch (error) {
-//     return thunkAPI.rejectWithValue(error.message);
-//   }
-// };
+const getCars = async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(`/adverts?page=${page}&limit=${LIMIT}`);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+};
 
-// export const getCarsThunk = createAsyncThunk('cars/getCars', getCars);
+export const getCarsThunk = createAsyncThunk('cars/getCars', getCars);
 
-// export const getAllCarsThunk = createAsyncThunk('cars/getAllCars', getAllCarss);
+export const getAllCarsThunk = createAsyncThunk('cars/getAllCars', getAllCarss);
